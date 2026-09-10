cf705a35764ea912bf49a7e8a22bfdf4eba66049
Simraa09 <simraa.parkar@gmail.com>
2026-09-11 00:05:02 +0530

    ver 1.0.2

--- analysis.py
+++ analysis.py
@@ -4,3 +4,9 @@ average_marks = sum(marks) / len(marks)
 
 print("Average Marks:", average_marks)
 print("Highest Marks:", max(marks))
+
+lowest_marks = min(marks)
+total_marks = sum(marks)
+
+print("Lowest Marks:", lowest_marks)
+print("Total Marks:", total_marks)
